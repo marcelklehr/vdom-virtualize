3175534199e61761865428709e8b954169b228ad
Marcel Klehr <mklehr@gmx.net>
2015-05-02 19:11:58 +0200

    Add standalone browserified bundle.js

---
+++ bundle.js
@@ -0,0 +1,385 @@
+!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.vdomVirtualize=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+/*!
+* vdom-virtualize
+* Copyright 2014 by Marcel Klehr <mklehr@gmx.net>
+*
+* (MIT LICENSE)
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+var VNode = _dereq_("virtual-dom/vnode/vnode")
+  , VText = _dereq_("virtual-dom/vnode/vtext")
+
+module.exports = createVNode
+
+function createVNode(domNode, key) {
+  key = key || null // XXX: Leave out `key` for now... merely used for (re-)ordering
+
+  if(domNode.nodeType == 1) return createFromElement(domNode, key)
+  if(domNode.nodeType == 3) return createFromTextNode(domNode, key)
+  return
+}
+
+createVNode.fromHTML = function(html, key) {
+  var domNode = document.createElement('div'); // create container
+  domNode.innerHTML = html; // browser parses HTML into DOM tree
+  domNode = domNode.children[0] || domNode; // select first node in tree
+  return createVNode(domNode, key);
+};
+
+function createFromTextNode(tNode) {
+  return new VText(tNode.nodeValue)
+}
+
+
+function createFromElement(el) {
+  var tagName = el.tagName
+  , namespace = el.namespaceURI == 'http://www.w3.org/1999/xhtml'? null : el.namespaceURI
+  , properties = getElementProperties(el)
+  , children = []
+
+  for (var i = 0; i < el.childNodes.length; i++) {
+    children.push(createVNode(el.childNodes[i]/*, i*/))
+  }
+
+  return new VNode(tagName, properties, children, null, namespace)
+}
+
+
+function getElementProperties(el) {
+  var obj = {}
+
+  props.forEach(function(propName) {
+    if(!el[propName]) return
+
+    // Special case: style
+    // .style is a DOMStyleDeclaration, thus we need to iterate over all
+    // rules to create a hash of applied css properties.
+    //
+    // You can directly set a specific .style[prop] = value so patching with vdom
+    // is possible.
+    if("style" == propName) {
+      var css = {}
+        , styleProp
+      for(var i=0; i<el.style.length; i++) {
+        styleProp = el.style[i]
+        css[styleProp] = el.style.getPropertyValue(styleProp) // XXX: add support for "!important" via getPropertyPriority()!
+      }
+
+      obj[propName] = css
+      return
+    }
+
+    // Special case: dataset
+    // we can iterate over .dataset with a simple for..in loop.
+    // The all-time foo with data-* attribs is the dash-snake to camelCase
+    // conversion.
+    // However, I'm not sure if this is compatible with h()
+    //
+    // .dataset properties are directly accessible as transparent getters/setters, so
+    // patching with vdom is possible.
+    if("dataset" == propName) {
+      var data = {}
+      for(var p in el.dataset) {
+        data[p] = el.dataset[p]
+      }
+
+      obj[propName] = data
+      return
+    }
+
+    // Special case: attributes
+    // some properties are only accessible via .attributes, so
+    // that's what we'd do, if vdom-create-element could handle this.
+    if("attributes" == propName) return
+    if("tabIndex" == propName && el.tabIndex === -1) return
+
+
+    // default: just copy the property
+    obj[propName] = el[propName]
+    return
+  })
+
+  return obj
+}
+
+/**
+ * DOMNode property white list
+ * Taken from https://github.com/Raynos/react/blob/dom-property-config/src/browser/ui/dom/DefaultDOMPropertyConfig.js
+ */
+var props =
+
+module.exports.properties = [
+ "accept"
+,"accessKey"
+,"action"
+,"alt"
+,"async"
+,"autoComplete"
+,"autoPlay"
+,"cellPadding"
+,"cellSpacing"
+,"checked"
+,"className"
+,"colSpan"
+,"content"
+,"contentEditable"
+,"controls"
+,"crossOrigin"
+,"data"
+,"dataset"
+,"defer"
+,"dir"
+,"download"
+,"draggable"
+,"encType"
+,"formNoValidate"
+,"href"
+,"hrefLang"
+,"htmlFor"
+,"httpEquiv"
+,"icon"
+,"id"
+,"label"
+,"lang"
+,"list"
+,"loop"
+,"max"
+,"mediaGroup"
+,"method"
+,"min"
+,"multiple"
+,"muted"
+,"name"
+,"noValidate"
+,"pattern"
+,"placeholder"
+,"poster"
+,"preload"
+,"radioGroup"
+,"readOnly"
+,"rel"
+,"required"
+,"rowSpan"
+,"sandbox"
+,"scope"
+,"scrollLeft"
+,"scrolling"
+,"scrollTop"
+,"selected"
+,"span"
+,"spellCheck"
+,"src"
+,"srcDoc"
+,"srcSet"
+,"start"
+,"step"
+,"style"
+,"tabIndex"
+,"target"
+,"title"
+,"type"
+,"value"
+
+// Non-standard Properties
+,"autoCapitalize"
+,"autoCorrect"
+,"property"
+
+, "attributes"
+]
+
+var attrs =
+module.exports.attrs = [
+ "allowFullScreen"
+,"allowTransparency"
+,"charSet"
+,"cols"
+,"contextMenu"
+,"dateTime"
+,"disabled"
+,"form"
+,"frameBorder"
+,"height"
+,"hidden"
+,"maxLength"
+,"role"
+,"rows"
+,"seamless"
+,"size"
+,"width"
+,"wmode"
+
+// SVG Properties
+,"cx"
+,"cy"
+,"d"
+,"dx"
+,"dy"
+,"fill"
+,"fx"
+,"fy"
+,"gradientTransform"
+,"gradientUnits"
+,"offset"
+,"points"
+,"r"
+,"rx"
+,"ry"
+,"spreadMethod"
+,"stopColor"
+,"stopOpacity"
+,"stroke"
+,"strokeLinecap"
+,"strokeWidth"
+,"textAnchor"
+,"transform"
+,"version"
+,"viewBox"
+,"x1"
+,"x2"
+,"x"
+,"y1"
+,"y2"
+,"y"
+]
+
+},{"virtual-dom/vnode/vnode":7,"virtual-dom/vnode/vtext":8}],2:[function(_dereq_,module,exports){
+module.exports = isThunk
+
+function isThunk(t) {
+    return t && t.type === "Thunk"
+}
+
+},{}],3:[function(_dereq_,module,exports){
+module.exports = isHook
+
+function isHook(hook) {
+    return hook &&
+      (typeof hook.hook === "function" && !hook.hasOwnProperty("hook") ||
+       typeof hook.unhook === "function" && !hook.hasOwnProperty("unhook"))
+}
+
+},{}],4:[function(_dereq_,module,exports){
+var version = _dereq_("./version")
+
+module.exports = isVirtualNode
+
+function isVirtualNode(x) {
+    return x && x.type === "VirtualNode" && x.version === version
+}
+
+},{"./version":6}],5:[function(_dereq_,module,exports){
+module.exports = isWidget
+
+function isWidget(w) {
+    return w && w.type === "Widget"
+}
+
+},{}],6:[function(_dereq_,module,exports){
+module.exports = "2"
+
+},{}],7:[function(_dereq_,module,exports){
+var version = _dereq_("./version")
+var isVNode = _dereq_("./is-vnode")
+var isWidget = _dereq_("./is-widget")
+var isThunk = _dereq_("./is-thunk")
+var isVHook = _dereq_("./is-vhook")
+
+module.exports = VirtualNode
+
+var noProperties = {}
+var noChildren = []
+
+function VirtualNode(tagName, properties, children, key, namespace) {
+    this.tagName = tagName
+    this.properties = properties || noProperties
+    this.children = children || noChildren
+    this.key = key != null ? String(key) : undefined
+    this.namespace = (typeof namespace === "string") ? namespace : null
+
+    var count = (children && children.length) || 0
+    var descendants = 0
+    var hasWidgets = false
+    var hasThunks = false
+    var descendantHooks = false
+    var hooks
+
+    for (var propName in properties) {
+        if (properties.hasOwnProperty(propName)) {
+            var property = properties[propName]
+            if (isVHook(property) && property.unhook) {
+                if (!hooks) {
+                    hooks = {}
+                }
+
+                hooks[propName] = property
+            }
+        }
+    }
+
+    for (var i = 0; i < count; i++) {
+        var child = children[i]
+        if (isVNode(child)) {
+            descendants += child.count || 0
+
+            if (!hasWidgets && child.hasWidgets) {
+                hasWidgets = true
+            }
+
+            if (!hasThunks && child.hasThunks) {
+                hasThunks = true
+            }
+
+            if (!descendantHooks && (child.hooks || child.descendantHooks)) {
+                descendantHooks = true
+            }
+        } else if (!hasWidgets && isWidget(child)) {
+            if (typeof child.destroy === "function") {
+                hasWidgets = true
+            }
+        } else if (!hasThunks && isThunk(child)) {
+            hasThunks = true;
+        }
+    }
+
+    this.count = count + descendants
+    this.hasWidgets = hasWidgets
+    this.hasThunks = hasThunks
+    this.hooks = hooks
+    this.descendantHooks = descendantHooks
+}
+
+VirtualNode.prototype.version = version
+VirtualNode.prototype.type = "VirtualNode"
+
+},{"./is-thunk":2,"./is-vhook":3,"./is-vnode":4,"./is-widget":5,"./version":6}],8:[function(_dereq_,module,exports){
+var version = _dereq_("./version")
+
+module.exports = VirtualText
+
+function VirtualText(text) {
+    this.text = String(text)
+}
+
+VirtualText.prototype.version = version
+VirtualText.prototype.type = "VirtualText"
+
+},{"./version":6}]},{},[1])
+(1)
+});
