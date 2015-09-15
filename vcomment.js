c98fb6f20489df6d84305885cbb5c9ca7383e5d5
Tim Kendrick <timkendrick@gmail.com>
2015-09-15 17:15:10 +0100

    Parse HTML comment nodes

---
+++ vcomment.js
@@ -0,0 +1,16 @@
+module.exports = VirtualComment
+
+function VirtualComment(text) {
+  this.text = String(text)
+}
+
+VirtualComment.prototype.type = 'Widget'
+
+VirtualComment.prototype.init = function() {
+  return document.createComment(this.text)
+}
+
+VirtualComment.prototype.update = function(previous, domNode) {
+  if(this.text === previous.text) return
+  domNode.nodeValue = this.text
+}
